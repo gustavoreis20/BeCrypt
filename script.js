cfa51dc7b8b054acf289b832639e1bc97e8a9cc0
Gustavo Reis Silva <49646779+CheeseBacon190@users.noreply.github.com>
2020-08-20 20:54:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
 const buttonElement = document.getElementById('button-element')
 
 
-function start(){
-    let coinSelected = document.getElementById('coin-selector').value
+function makeRequest(coin,getLink){
+    const url = getLink(coin)
+    const request = new XMLHttpRequest()
 
+    request.open("GET", url)
+    request.responseType = 'text'
+    request.send()
 
-    const getLink = coinSelected => `https://economia.awesomeapi.com.br/json/all/${coinSelected}-BRL`
-
-
-    function makeRequest(coin,getLink){
-        const url = getLink(coin)
-        const request = new XMLHttpRequest()
-
-        request.open("GET", url)
-        request.responseType = 'text'
-        request.send()
-
-        request.onload = () => {
-            if (request.status >= 200 && request.status < 300) {
-                const response = JSON.parse(request.responseText)
-                console.log(response)
-                
-                for(let key in response){
-                    if(typeof response[key] === 'object'){
-                        const criptCoin = {...response[key]}
-                        document.querySelector('p').innerHTML = `name: ${criptCoin.name} <br> valor atual: ${criptCoin.ask}`
-                    }
-                    else console.log('error')
+    request.onload = () => {
+        if (request.status >= 200 && request.status < 300) {
+            const response = JSON.parse(request.responseText)
+            console.log(response)
+            
+            for(let key in response){
+                if(typeof response[key] === 'object'){
+                    const criptCoin = {...response[key]}
+                    document.querySelector('p').innerHTML = `name: ${criptCoin.name} <br> valor atual: ${criptCoin.ask} <br> alta: ${criptCoin.high} <br> baixa: ${criptCoin.low}`
                 }
+                else console.log('error')
             }
         }
-
-        return request.onload()
     }
+
+    return request.onload()
+}
+
+
+function start(){
+    let coinSelected = document.getElementById('coin-selector').value
+    const getLink = coinSelected => `https://economia.awesomeapi.com.br/json/all/${coinSelected}-BRL`
 
     makeRequest(coinSelected, getLink)
 }
 
+buttonElement.onclick = start
